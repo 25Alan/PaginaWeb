ea5fc197cbafbb11be6b1c86b84ffb5b0366de6b
25Alan <romeroalan71@gmail.com>
2022-11-29 12:39:57 -0300

    AGREGADO LA FUNCION DE BOTONES DEL CUADRO COMPRA - FALTA AGREGARLE A LA TABLA MAINTENANCE

--- Js/user/image.js
+++ Js/user/image.js
@@ -1,5 +1,7 @@
 const link = document.querySelector('.clickDescription');
 const buttonOk = document.querySelector('.buttonOk');
+const button_buy = document.querySelector('.button_buy');
+const buttonConfirm = document.querySelector('.buttonConfirm');
 
 link.addEventListener('click', () => {
     document.querySelector('.containerDescription').style.display = 'block';
@@ -7,4 +9,12 @@ link.addEventListener('click', () => {
 
 buttonOk.addEventListener('click', () => {
     document.querySelector('.containerDescription').style.display = 'none';    
+});
+
+button_buy.addEventListener('click', () => {
+    document.querySelector('.containerBuy').style.display = 'block';
+});
+
+buttonConfirm.addEventListener('click', () => {
+    document.querySelector('.containerBuy').style.display = 'none';
 });
